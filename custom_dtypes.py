f75b7137d66a4a30fb64068cb2abe9a9da99527f
Woody Sims <sims.woodrow@gmail.com>
2022-12-15 23:52:24 -0600

    custom dtypes!

--- custom_dtypes.py
+++ custom_dtypes.py
@@ -5,9 +5,11 @@ import pandas as pd
 from numpy.typing import ArrayLike
 from sklearn.linear_model import LassoLars, LinearRegression, TweedieRegressor
 from sklearn.preprocessing import MinMaxScaler, RobustScaler
+from wood_steel_regressor import WoodSteelRegression
 
 ModelDataType = t.Union[ArrayLike, pd.DataFrame, pd.Series]
-LinearRegressionType = t.Union[LinearRegression, LassoLars, TweedieRegressor]
+LinearRegressionType = t.Union[LinearRegression, LassoLars,
+                               TweedieRegressor, WoodSteelRegression]
 PandasDataType = t.Union[pd.Series, pd.DataFrame]
 ScalerType = t.Union[MinMaxScaler, RobustScaler]
 lmplot_kwargs = {'scatter': {'color': '#40b7ad'}, 'line': {'color': '#2e1e3b'}}
